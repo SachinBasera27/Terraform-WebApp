9dadef71c9ff66a281f2f7047242d9aba52ccd5c
SachinBasera27 <sachinbasera.recruit@gmail.com>
2026-07-21 03:10:02 +0530

    Added the portion for sas config

--- main.tf
+++ main.tf
@@ -6,19 +6,37 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_service_plan" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_service_plan" "sp-tf" {
+  name                = "terraform_servicePlan"
+  resource_group_name = module.resource.name
+  location            = module.resource.location
   os_type             = "Linux"
-  sku_name            = "P1v2"
+  sku_name            = "P1v3"
 }
 
 resource "azurerm_linux_web_app" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_service_plan.example.location
-  service_plan_id     = azurerm_service_plan.example.id
+  name                = "webapp"
+  resource_group_name = module.resource.name
+  location            = azurerm_service_plan.sp-tf.location
+  service_plan_id     = azurerm_service_plan.sp-tf.id
 
-  site_config {}
+  site_config {
+    ftps_state = "AllAllowed"
+    worker_count = 2
+
+  }
+
+  logs{
+    application_logs {
+      file_system_level = "Warning"
+      azure_blob_storage {
+        level = "Warning"
+        retention_in_days = 7
+        sas_url = "https://${azurerm_storage_account.stacc-tf-webapp.name}.blob.core.windows.net/${azurerm_storage_container.cont-tf-webapp.name}${module.data.sas_url_query_string}&sr=b}"
+      }
+    }
+  }
 }
+
+
+
--- resource/resourceMain.tf
+++ resource/resourceMain.tf
@@ -2,3 +2,21 @@ resource "azurerm_resource_group" "RG-GH-Terraform" {
   name     = "rg-terraform-webapp"
   location = "Central US"
 }
+
+resource "azurerm_storage_account" "stacc-tf-webapp" {
+  name                     = "stacctf-webapp"
+  resource_group_name      = azurerm_resource_group.RG-GH-Terraform.name
+  location                 = azurerm_resource_group.RG-GH-Terraform.location
+  account_tier             = "Standard"
+  account_kind             = "BlobStorage"
+  access_tier              = "Hot"
+  account_replication_type = "GRS"
+  shared_access_key_enabled = true
+  public_network_access_enabled = true
+}
+
+resource "azurerm_storage_container" "cont-tf-webapp" {
+  name                  = "tf-prod-webapp"
+  storage_account_id    = azurerm_storage_account.stacc-tf-webapp.id
+  container_access_type = "private"
+}
